cd081674a9d6470845e6c699db141a4efc5e892e
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-11 13:45:21 -0700

    setup new ice cream form

--- src/components/NewIceCreamForm.js
+++ src/components/NewIceCreamForm.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+import { v4 } from "uuid";
 
 function NewIceCreamForm(props) {
+  function handleNewIceCreamFormSubmission(event) {
+    event.preventDefault();
+    props.onNewIceCreamCreation({
+      flavor: event.target.flavor.value,
+      price: parseInt(event.target.price.value),
+      description: event.target.decription.value,
+      quantity: parseInt(event.target.quantity.value),
+      id: v4(),
+    });
+  }
+
   return (
-    <div>NewIceCreamForm</div>
-  )
+    <React.Fragment>
+      <h1>- Add Ice Cream -</h1>
+      <ReusableForm
+        formSubmissionHandler={handleNewIceCreamFormSubmission}
+        buttonText="Add Ice Cream"
+      />
+    </React.Fragment>
+  );
 }
 
-NewIceCreamForm.propTypes = {}
+NewIceCreamForm.propTypes = {
+  onNewIceCreamCreation: PropTypes.func,
+};
 
-export default NewIceCreamForm
-
+export default NewIceCreamForm;
