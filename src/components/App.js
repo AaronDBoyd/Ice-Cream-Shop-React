90ac62b02a04af36548768e877eb70da1e86cd05
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-11 13:27:10 -0700

    import IceCreamControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import IceCreamControl from './IceCreamControl';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <IceCreamControl />
     </React.Fragment>
   );
 }
