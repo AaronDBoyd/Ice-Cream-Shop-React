87f58a989a8b5cd540f403e1121399268b58d031
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-11 13:56:57 -0700

    setup ice cream component

--- src/components/IceCream.js
+++ src/components/IceCream.js
@@ -2,11 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function IceCream(props) {
+  let buyButtonVisible = null;
+  let buttonText = null;
+
+  if (props.quantity > 0) {
+    buyButtonVisible = (() => props.whenBuyClicked(props.id));
+    buttonText = "Buy Pint";
+  } else {
+    buttonText = "Out of Stock";
+  }
+
   return (
-    <div>IceCream</div>
+    <React.Fragment>
+      <div onClick = {() => props.whenIceCreamClicked(props.id)}> 
+        <h3>{props.flavor}</h3>
+        <h3>${props.price}</h3>
+        <h4>{props.description}</h4>
+        <h4>Pints Left: {props.quantity}</h4>
+      </div>
+      <button onClick={buyButtonVisible}>{buttonText}</button>
+      <hr />
+    </React.Fragment>
   )
 }
 
-IceCream.propTypes = {}
+IceCream.propTypes = {
+  flavor: PropTypes.string,
+  description: PropTypes.string,
+  quantity: PropTypes.number,
+  id: PropTypes.string,
+  whenIceCreamClicked: PropTypes.func,
+  whenBuyClicked: PropTypes.func,
+}
 
 export default IceCream
