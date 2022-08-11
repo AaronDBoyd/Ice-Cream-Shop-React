9c0c3de26079fb69ee70cd944b2ac6a61c941cac
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-11 13:51:03 -0700

    setup edit ice cream form

--- src/components/EditIceCreamForm.js
+++ src/components/EditIceCreamForm.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReusableForm from './ReusableForm'
 
 function EditIceCreamForm(props) {
+  const { iceCream } = props;
+
+  function handleEditIceCreamFormSubmission(event) {
+    event.preventDefault();
+    props.onEditIceCream({
+      flavor: event.target.flavor.value,
+      price: parseInt(event.target.price.value),
+      description: event.target.description.value,
+      quantity: parseInt(event.target.quantity.value),
+      id: iceCream.id
+    })
+  }
+
   return (
-    <div>EditIceCreamForm</div>
+    <React.Fragment>
+      <h1>- Edit Ice Cream -</h1>
+      <ReusableForm 
+        formSubmissionHandler={handleEditIceCreamFormSubmission}
+        buttonText="Update Ice Cream"
+      />
+    </React.Fragment>
   )
 }
 
-EditIceCreamForm.propTypes = {}
+EditIceCreamForm.propTypes = {
+  iceCream: PropTypes.object,
+  onEditIceCream: PropTypes.func
+}
 
 export default EditIceCreamForm
 
