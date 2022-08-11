4a03ee4762d84de9b997c1aa7739b09151ba2236
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-11 14:03:36 -0700

    setup ice cream list component

--- src/components/IceCreamList.js
+++ src/components/IceCreamList.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import IceCream from "./IceCream";
 
 function IceCreamList(props) {
   return (
-    <div>IceCreamList</div>
-  )
+    <React.Fragment>
+      <h2>Ice Cream Selection</h2>
+      <hr />
+      {props.iceCreamList.map((iceCream) => (
+        <IceCream
+          whenBuyClicked={props.onClickingBuy}
+          whenIceCreamClicked={props.onIceCreamSelection}
+          flavor={iceCream.flavor}
+          price={iceCream.price}
+          description={iceCream.description}
+          quantity={iceCream.quantity}
+          id={iceCream.id}
+          key={iceCream.id}
+        />
+      ))}
+    </React.Fragment>
+  );
 }
 
-IceCreamList.propTypes = {}
+IceCreamList.propTypes = {
+  iceCreamList: PropTypes.array,
+  onIceCreamSelection: PropTypes.func,
+  onClickingBuy: PropTypes.func,
+};
 
-export default IceCreamList
+export default IceCreamList;
