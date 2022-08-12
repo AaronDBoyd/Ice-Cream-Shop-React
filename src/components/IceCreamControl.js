f1f1f05f73449f63b8f3f12b0ac34bccb598e95c
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-11 23:57:04 -0700

    implement constructor

--- src/components/IceCreamControl.js
+++ src/components/IceCreamControl.js
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import IceCreamList from './IceCreamList';
+import IceCreamDetail from './IceCreamDetail';
+import NewIceCreamForm from './NewIceCreamForm';
+import EditIceCreamForm from './EditIceCreamForm';
 
 class IceCreamControl extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      selectedIceCream: null,
+      mainIceCreamList: [],
+      editing: false,
+    };
+  }
 
 
 
